be60b3cafc89e531ef5c24926744d0fa7afd6d6e
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-06-25 17:53:46 +0300

    Add a solution

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -2,6 +2,13 @@ from urllib.request import Request
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from photos.models import Photo
+
 
 def home_page_view(request: Request) -> HttpResponse:
-    return render(request, 'common/home-page.html')
+    all_photos = Photo.objects.all()
+
+    context = {
+        'all_photos': all_photos,
+    }
+    return render(request, 'common/home-page.html', context)
