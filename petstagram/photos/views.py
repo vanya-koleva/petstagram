de4bb3310c407e784c60eff6f8a67e198cb88826
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-06-26 17:13:03 +0300

    Add a solution

--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -2,13 +2,21 @@ from urllib.request import Request
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from photos.models import Photo
+
 
 def photo_add_view(request: Request) -> HttpResponse:
     return render(request, 'photos/photo-add-page.html')
 
 
 def photo_details_view(request: Request, pk: int) -> HttpResponse:
-    return render(request, 'photos/photo-details-page.html')
+    photo = Photo.objects.get(pk=pk)
+
+    context = {
+        'photo': photo,
+    }
+
+    return render(request, 'photos/photo-details-page.html', context)
 
 
 def photo_edit_view(request: Request, pk: int) -> HttpResponse:
