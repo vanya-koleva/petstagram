37a6baf310bfe4b121ffe8d1f2a6299f7fab0175
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-06-26 18:17:05 +0300

    Add a solution

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -2,13 +2,23 @@ from urllib.request import Request
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from pets.models import Pet
+
 
 def pet_add_view(request: Request) -> HttpResponse:
     return render(request, 'pets/pet-add-page.html')
 
 
 def pet_details_view(request: Request, username: str, pet_slug: str) -> HttpResponse:
-    return render(request, 'pets/pet-details-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    all_photos = pet.photo_set.all()
+
+    context = {
+        'pet': pet,
+        'all_photos': all_photos,
+    }
+
+    return render(request, 'pets/pet-details-page.html', context)
 
 
 def pet_edit_view(request: Request, username: str, pet_slug: str) -> HttpResponse:
