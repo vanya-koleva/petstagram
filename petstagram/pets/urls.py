a48279bedcd501dbdd9a93cbeee970200e466749
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-07-02 18:18:22 +0300

    Add a solution

--- petstagram/pets/urls.py
+++ petstagram/pets/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('add/', views.PetAddView.as_view(), name='add-pet'),
     path('<str:username>/pet/<slug:pet_slug>/', include([
         path('', views.PetDetailsView.as_view(), name='pet-details'),
-        path('edit/', views.pet_edit_view, name='edit-pet'),
+        path('edit/', views.PetEditView.as_view(), name='edit-pet'),
         path('delete/', views.pet_delete_view, name='delete-pet'),
     ]))
 ]
