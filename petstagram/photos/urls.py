ddd277d789f6feebef2a1b89c72c5448fb79ef93
Vanya Koleva <148770184+vanya-koleva@users.noreply.github.com>
2025-07-06 04:12:09 +0300

    Add a solution

--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -5,7 +5,7 @@ from photos import views
 urlpatterns = [
     path('add/', views.PhotoAddView.as_view(), name='add-photo'),
     path('<int:pk>/', include([
-        path('', views.photo_details_view, name='photo-details'),
+        path('', views.PhotoDetailView.as_view(), name='photo-details'),
         path('edit/', views.photo_edit_view, name='edit-photo'),
         path('delete/', views.photo_delete_view, name='delete-photo'),
     ])),
--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from common.forms import CommentForm
 from photos.forms import PhotoCreateForm, PhotoEditForm
@@ -15,18 +15,16 @@ class PhotoAddView(CreateView):
     success_url = reverse_lazy('home')
 
 
-def photo_details_view(request: HttpRequest, pk: int) -> HttpResponse:
-    photo = Photo.objects.get(pk=pk)
-    comments = photo.comment_set.all()
-    comment_form = CommentForm()
+class PhotoDetailView(DetailView):
+    model = Photo
+    template_name = 'photos/photo-details-page.html'
 
-    context = {
-        'photo': photo,
-        'comments': comments,
-        'comment_form': comment_form,
-    }
-
-    return render(request, 'photos/photo-details-page.html', context)
+    def get_context_data(self, **kwargs):
+        kwargs.update({
+            'comments': self.object.comment_set.all(),
+            'comment_form': CommentForm(),
+        })
+        return super().get_context_data(**kwargs)
 
 
 def photo_edit_view(request: HttpRequest, pk: int) -> HttpResponse:
